95e7d6a15b30eeb5fb762a5dbf0722fe5d7d7d78
tetsu-sh <tetsusinmyou@yahoo.co.jp>
2022-12-07 22:08:51 +0900

    asset path

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 module.exports={
     reactStrictMode:true,
     basePath:process.env.GITHUB_ACTIONS && "/my-blog",
+    assetPrefix:this.basePath,
     trailingSlash:true,
     images:{unoptimized:true},
 }
