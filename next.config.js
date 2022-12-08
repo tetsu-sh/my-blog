761241e22005b36cb2327fb827fb26ab188a78a8
tetsu-sh <tetsusinmyou@yahoo.co.jp>
2022-12-08 18:54:51 +0900

    env github actions

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 module.exports={
     reactStrictMode:true,
     basePath:process.env.GITHUB_ACTIONS ? '/my-blog':'',
-    assetPrefix:'/',
+    assetPrefix:this.basePath,
     trailingSlash:true,    
     images:{unoptimized:true},
 }
