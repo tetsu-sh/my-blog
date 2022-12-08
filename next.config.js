47b32bae273a468f717da7898072362364ac88ac
tetsu-sh <tetsusinmyou@yahoo.co.jp>
2022-12-08 17:42:36 +0900

    image optimization

--- next.config.js
+++ next.config.js
@@ -1,7 +1,9 @@
-module.exports={
+const withExportImages=require('next-export-optimize-images')
+
+module.exports=withExportImages({
     reactStrictMode:true,
     basePath:process.env.GITHUB_ACTIONS && "/my-blog",
     assetPrefix:this.basePath,
-    trailingSlash:true,
-    images:{unoptimized:true},
-}
+    trailingSlash:true,    
+    images:{domains:this.basePath},
+})
