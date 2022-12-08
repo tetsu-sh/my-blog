40fab0c4833dde992833e9af5f9b580b33333f18
tetsu-sh <tetsusinmyou@yahoo.co.jp>
2022-12-08 18:40:37 +0900

    prefix

--- next.config.js
+++ next.config.js
@@ -1,8 +1,8 @@
 
 module.exports={
     reactStrictMode:true,
-    basePath:process.env.GITHUB_ACTIONS ? "/my-blog":"",
-    assetPrefix:this.basePath,
+    basePath:process.env.GITHUB_ACTIONS ? '/my-blog':'',
+    assetPrefix:process.env.GITHUB_ACTIONS? '/my-blog':'',
     trailingSlash:true,    
     images:{unoptimized:true},
 }
