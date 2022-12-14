f9b31660d35f2be45cc13ba10bb469930c25296b
tetsu-sh <tetsusinmyou@yahoo.co.jp>
2022-12-14 13:58:47 +0900

    footerのsnsアイコンを直した

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,40 +1,44 @@
-import Container from './container'
-import { EXAMPLE_PATH } from '../lib/constants'
-import Link from 'next/link'
-import {SiGithub,SiTwitter} from 'react-icons/si'
+import Container from "./container";
+import { EXAMPLE_PATH } from "../lib/constants";
+import Link from "next/link";
+import { SiGithub, SiTwitter } from "react-icons/si";
 
-const sns=[
-  {href: 'https://github.com/tetsu-sh',
-  icon: <SiGithub size={20} />,
-  label: 'Github',},{href: 'https://twitter.com/tetsu04228',
-  icon: <SiTwitter size={20} />,
-  label: 'Twitter',}
-]
+const sns = [
+  {
+    href: "https://github.com/tetsu-sh",
+    icon: <SiGithub size={20} />,
+    label: "Github",
+  },
+  {
+    href: "https://twitter.com/tetsu04228",
+    icon: <SiTwitter size={20} />,
+    label: "Twitter",
+  },
+];
 const Footer = () => {
   return (
     <footer>
-<div className='p-10 center bg-neutral-800'>
-  
-<div className="vstack items-center gap-4">
+      <div className="p-10 center bg-neutral-800">
+        <div className="flex flex-col items-center gap-4">
           <div className="flex gap-4">
             {sns.map(({ href, icon, label }) => (
-              <Link key={href} href={href} className="text-neutral-200 hover:text-neutral-500 active:text-gray-600 transition duration-100">
-
+              <Link key={href} href={href} passHref>
+                <span
+                  className="text-neutral-200 hover:text-neutral-500 active:text-gray-600 transition duration-100"
+                  aria-label={label}
+                >
                   {icon}
-                              </Link>
+                </span>
+              </Link>
             ))}
           </div>
           <div className="text-neutral-200 text-sm text-center">
-      © {new Date().getFullYear()} Chimu
+            © {new Date().getFullYear()} Chimu
           </div>
         </div>
-</div>
+      </div>
+    </footer>
+  );
+};
 
-
-
-
-  </footer>
-  )
-}
-
-export default Footer
+export default Footer;
