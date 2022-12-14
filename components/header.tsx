e21c9799d6b54ede0d2a70c4e17d5ee354bc50ad
tetsu-sh <tetsusinmyou@yahoo.co.jp>
2022-12-14 14:02:59 +0900

    header 修正

--- components/header.tsx
+++ components/header.tsx
@@ -1,31 +1,31 @@
-import Link from 'next/link'
+import Link from "next/link";
 
 const Header = () => {
   return (
     <header>
-    <nav
-      className="fixed z-20 w-full bg-gray-700 py-3 text-center font-semibold"
-      aria-label="Header navigation"
-    >
-      <Link
-        href="/"
-        className="rounded px-5 py-2.5 font-mono tracking-tight no-underline hover:bg-gray-700/50 active:bg-gray-600"
+      <nav
+        className="fixed z-20 w-full bg-gray-700 py-3 text-center font-semibold"
+        aria-label="Header navigation"
       >
-        <span className='test-xs px-5 test-gray-100 duration-100'>
-        home
-        </span>
-      </Link>
-      <Link
-      href="/about"
-      className="rounded px-5 py-2.5 font-mono tracking-tight no-underline hover:bg-gray-700/50 active:bg-gray-600">
-      
-      <span className='test-xs test-gray-100 duration-100'>
-        about
-        </span> 
-      </Link>
-    </nav>
-  </header>
-  )
-}
+        <Link
+          href="/"
+          className="rounded px-5 py-2.5 font-mono tracking-tight no-underline hover:bg-gray-700/50 active:bg-gray-600"
+        >
+          <span className="test-xs px-5 text-neutral-200 hover:text-neutral-500 active:text-gray-600 transition duration-100">
+            home
+          </span>
+        </Link>
+        <Link
+          href="/about"
+          className="rounded px-5 py-2.5 font-mono tracking-tight no-underline hover:bg-gray-700/50 active:bg-gray-600"
+        >
+          <span className="test-xs text-neutral-200 hover:text-neutral-500 active:text-gray-600 transition duration-100">
+            about
+          </span>
+        </Link>
+      </nav>
+    </header>
+  );
+};
 
-export default Header
+export default Header;
