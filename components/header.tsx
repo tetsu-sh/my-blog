aa4a0ce4b33e498058023a205a0daef2dab12ea1
tetsu-sh <tetsusinmyou@yahoo.co.jp>
2023-01-06 11:07:33 +0900

    resize

--- components/header.tsx
+++ components/header.tsx
@@ -11,7 +11,7 @@ const Header = () => {
           href="/"
           className="rounded px-5 py-2.5 font-mono tracking-tight no-underline hover:bg-gray-700/50 active:bg-gray-600"
         >
-          <span className="test-xs px-5 text-neutral-200 hover:text-neutral-500 active:text-gray-600 transition duration-100">
+          <span className="test-xs px-2.5 text-neutral-200 hover:text-neutral-500 active:text-gray-600 transition duration-100">
             home
           </span>
         </Link>
@@ -19,7 +19,7 @@ const Header = () => {
           href="/about"
           className="rounded px-5 py-2.5 font-mono tracking-tight no-underline hover:bg-gray-700/50 active:bg-gray-600"
         >
-          <span className="test-xs px-5 text-neutral-200 hover:text-neutral-500 active:text-gray-600 transition duration-100">
+          <span className="test-xs px-2.5 text-neutral-200 hover:text-neutral-500 active:text-gray-600 transition duration-100">
             about
           </span>
         </Link>
@@ -27,7 +27,7 @@ const Header = () => {
           href="/oisource"
           className="rounded px-5 py-2.5 font-mono tracking-tight no-underline hover:bg-gray-700/50 active:bg-gray-600"
         >
-          <span className="test-xs text-neutral-200 hover:text-neutral-500 active:text-gray-600 transition duration-100">
+          <span className="test-xs px-2.5 text-neutral-200 hover:text-neutral-500 active:text-gray-600 transition duration-100">
             oisource
           </span>
         </Link>
